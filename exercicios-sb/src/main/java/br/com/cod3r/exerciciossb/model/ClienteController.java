9cb3e4a6f9bac320791b0e1ea281bb9018095672
Luiz Antonio Carlin <luiz.carlin@gmail.com>
2021-01-27 10:02:28 -0300

    Commit Parcial 2m 2021-01-27
    Até a aula 426

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/ClienteController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/ClienteController.java
@@ -1,10 +1,12 @@
-package br.com.cod3r.exerciciossb.models;
+package br.com.cod3r.exerciciossb.model;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cod3r.exerciciossb.model.entities.Cliente;
 
 @RestController
 @RequestMapping(path = "/clientes")
