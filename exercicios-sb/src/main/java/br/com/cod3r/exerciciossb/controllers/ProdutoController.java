23dafd066246a96e720ac6baa25e1e325db5cf49
Luiz Antonio Carlin <luiz.carlin@gmail.com>
2021-01-30 14:25:42 -0300

    2021-01-27 parcial
    APIs Spring boot

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -1,9 +1,17 @@
 package br.com.cod3r.exerciciossb.controllers;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,15 +23,46 @@ import br.com.cod3r.exerciciossb.model.repositories.ProdutoRepository;
 public class ProdutoController {
 	@Autowired
 	private ProdutoRepository produtoRepository ;
-
-	@PostMapping
-	public @ResponseBody  Produto novoProduto( @RequestParam  String nome ,
-			@RequestParam  double  preco ,
-			@RequestParam  double  desconto 
-			) { 
-		Produto produto = new Produto (nome , preco, desconto ) ;
+	
+//	@PostMapping
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT} )
+	public @ResponseBody  Produto salvarProduto(@Valid Produto produto ) { 
+		// Produto produto = new Produto (nome , preco, desconto ) ;
 		produtoRepository.save(produto);
 		return produto ; 
+	}
+	
+	@GetMapping
+	public Iterable<Produto> obterProdutos () {
+		return produtoRepository.findAll();
+	}
+	
+	@GetMapping(path = "nome/{parteNome}")
+	public Iterable<Produto> obterProdutosPorNome (@PathVariable String parteNome ) {
+		return produtoRepository.findByNomeContainingIgnoreCase(parteNome);
+	}
+	
+	@GetMapping(path="/{id}")
+	public  Optional<Produto> obterProdutoPorId(@PathVariable int id ) {
+		return produtoRepository.findById(id);
+	}
+	
+//	@PutMapping
+//	public Produto alterarProduto (@Valid Produto produto ) {
+//		produtoRepository.save(produto);
+//		return produto ; 
+//	}
+	
+	@DeleteMapping (path = "/{id}")
+	public void excluirProduto (@PathVariable int id ) {
+		produtoRepository.deleteById(id);
+	}
+	
+	@GetMapping(path ="/pagina/{numeroPagina}" )
+	public Iterable<Produto> obterProdutosPorPagins( @PathVariable int numeroPagina ) {
+		Pageable page = PageRequest.of(numeroPagina, 3);
+		return produtoRepository.findAll(page); 
+		
 	}
 
 }
